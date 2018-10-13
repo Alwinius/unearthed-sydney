84eddb1134e449c97ac083b6b234de9c147905cb
Alwin <github@alwin.net.au>
2018-10-13 22:37:55 +1100

    added vehicles to map

--- js/startmin.js
+++ js/startmin.js
@@ -35,9 +35,33 @@ $(function() {
     }
 });
 
-var mymap = L.map('minemap').setView([-33.6389105, 147.3647931], 13);
+var mymap = L.map('minemap').setView([-33.6391572,147.4008518,3248], 15);
 
 L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
     maxZoom: 20,
     subdomains:['mt0','mt1','mt2','mt3']
 }).addTo(mymap);
+
+var truckIcon = L.icon({
+    iconUrl: '../css/truck_pin.png',
+    iconSize: [38, 44],
+    iconAnchor: [19, 44]
+});
+
+var diggerIcon = L.icon({
+    iconUrl: '../css/digger_pin.png',
+    iconSize: [38, 44],
+    iconAnchor: [19, 44]
+});
+
+L.marker([-33.633417,147.4039743,965], {icon: truckIcon}).addTo(mymap);
+L.marker([-33.634309, 147.410360,965], {icon:truckIcon}).addTo(mymap);
+L.marker([-33.630567,147.3842453,966], {icon: truckIcon}).addTo(mymap);
+L.marker([-33.647409,147.3951163,965], {icon: truckIcon}).addTo(mymap);
+L.marker([-33.638873, 147.404173,965], {icon: truckIcon}).addTo(mymap);
+L.marker([-33.643578, 147.405256,965], {icon: truckIcon}).addTo(mymap);
+L.marker([-33.637958, 147.399579,965], {icon: truckIcon}).addTo(mymap);
+
+L.marker([-33.636479, 147.405260,965], {icon: diggerIcon}).addTo(mymap);
+L.marker([-33.636740, 147.410111,1368], {icon: diggerIcon}).addTo(mymap);
+//L.marker([-33.633417,147.4039743,965], {icon: truckIcon}).addTo(mymap);
